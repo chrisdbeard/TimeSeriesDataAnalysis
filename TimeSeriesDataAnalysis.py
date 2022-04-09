6c5f301eb82126ba2ef0f04f32b5f08bd0d88364
chrisdbeard <88802276+chrisdbeard@users.noreply.github.com>
2022-04-09 10:21:20 -0400

    Update added class skeletons
    Added all the class skeletons as a start to the project.

--- TimeSeriesDataAnalysis.py
+++ TimeSeriesDataAnalysis.py
@@ -4,5 +4,80 @@ Created on Sat Apr  9 09:32:50 2022
 
 @author: Chris
 """
+import pathlib
 
-#TimeSeriesDataAnalysis
+
+#MainForm is a GUI. Need to make a GUI here or hard code, or prompt user in console for input?
+
+class DataEntry():
+    #properties?
+    
+    # parameterized constructor
+    def __init__(self):
+    
+    #Is there a need to override tostring in python?
+    def to_string():
+        
+ 
+class Event():
+    
+    
+    def __init__(self):
+        
+        
+    def parse_yes_no():
+        
+ 
+class DataSet():
+    
+    
+    def __init__(self):
+        
+        
+    def read_data_file():
+        
+        
+    def get_data_by_event():
+        
+
+class EventList():
+    
+    
+    def __init__(self):
+        
+        
+    def read_event_file():
+        
+
+class AnalysisController():
+    
+    
+    def __init__(self):
+        
+        
+    def read_data_file():
+        
+        
+    def read_event_file():
+        
+        
+    def seperate_data_files_by_event():
+        
+        
+    def write_data_files_by_event():
+        
+        
+    def clear():
+        
+        
+    
+    
+    
+
+
+#def Main():
+ 
+    
+ 
+#if __name__ == '__main__':
+#    main()
